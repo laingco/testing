236c14b87f070c5f1cf7b2cfe4c70ecaa1ab1a33
laingco <laingco@whs.school.nz>
2024-05-04 21:53:05 +1200

    Added nearby mine checking and bug fixes

--- src/GUI.java
+++ src/GUI.java
@@ -3,15 +3,18 @@ import java.awt.*;
 import java.awt.event.*;
 
 class gui implements MouseListener{
-    private static int difficulty = 2;
-    private static int gridSize[] = {8, 10, 14, 18, 20, 24};
-    private static int flagSize[] = {10, 40, 99};
-    private static int easyGrid[][] = new int[gridSize[0]][gridSize[1]];
-    private static int mediumGrid[][] = new int[gridSize[2]][gridSize[3]];
-    private static int hardGrid[][] = new int[gridSize[4]][gridSize[5]];
-    private static JFrame jframe;
-    private JButton tiles[][] = new JButton[20][24];
-    
+    private int difficulty = 2;
+    private int gridSize[] = {8, 10, 14, 18, 20, 24};
+    private int flagSize[] = {10, 40, 99};
+    private int easyGrid[][] = new int[gridSize[0]][gridSize[1]];
+    private int mediumGrid[][] = new int[gridSize[2]][gridSize[3]];
+    private int hardGrid[][] = new int[gridSize[4]][gridSize[5]];
+    private JFrame jframe = new JFrame("Minesweeper test");   //create JFrame object
+    private JButton tiles[][] = new JButton[20][24]; // -2 = safe to click -1 = bomb 0 = uncovered
+    private boolean flagged[][] = new boolean[20][24];
+    private int flags = 0;
+    private JLabel flagsLabel = new JLabel("Flags left: " + flags);
+
     ImageIcon temp1 = new ImageIcon("src/783503.png");
     Image image = temp1.getImage();
     Image newimg = image.getScaledInstance(15, 15,  java.awt.Image.SCALE_SMOOTH);
@@ -22,18 +25,27 @@ class gui implements MouseListener{
     Image newimg2 = image2.getScaledInstance(30, 30,  java.awt.Image.SCALE_SMOOTH);
     ImageIcon flagImage = new ImageIcon(newimg2); 
 
+    ImageIcon temp3 = new ImageIcon("src/Eo_circle_green_checkmark.svg.png");
+    Image image3 = temp3.getImage();
+    Image newimg3 = image3.getScaledInstance(30, 30,  java.awt.Image.SCALE_SMOOTH);
+    ImageIcon uncoveredButton = new ImageIcon(newimg3); 
+
     JButton reset = new JButton("Reset", resetImage);
     JMenuItem easy = new JMenuItem("Easy");
     JMenuItem medium = new JMenuItem("Medium");
     JMenuItem hard = new JMenuItem("Hard");
     JMenuItem whiteAndGrey = new JMenuItem("White and Grey (default)");
-    JPanel playArea = new JPanel();
     int elapsedTime = 0;
     JLabel time = new JLabel("Time: " + elapsedTime + "s");
-    Timer timer;
     boolean plantedMines = false;
 
-    public static String difficultyPicker(int choice){
+    Timer timer = new Timer(1000, new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            updateTimeLabel();
+        }
+    });
+
+    public String difficultyPicker(int choice){
         if (choice == 0){
             switch (difficulty) {
                 case 1:
@@ -50,7 +62,7 @@ class gui implements MouseListener{
         }
     }
 
-    public static int[][] gridPicker(){
+    public int[][] gridPicker(){
         switch (difficulty) {
             case 1:
                 return(easyGrid);
@@ -64,16 +76,15 @@ class gui implements MouseListener{
     }
     
     public void guiMethod(){
-        jframe = new JFrame("Minesweeper test");   //create JFrame object
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.setSize(1050,900);         //set size of GUI screen
-        jframe.setVisible(true);
+        jframe.getContentPane().removeAll();
 
         JPanel score = new JPanel();
         JPanel playArea = new JPanel();
         playArea.setLayout(new GridLayout(gridSize[2*difficulty-2], gridSize[2*difficulty-1]));
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setSize(1050,900);         //set size of GUI screen
+        jframe.setVisible(true);
 
-        JLabel flags = new JLabel("Flags left: " + flagSize[difficulty-1]);
         elapsedTime = 0;
         time.setText("Time: " + elapsedTime + "s");
         JMenuBar menu = new JMenuBar();
@@ -81,20 +92,25 @@ class gui implements MouseListener{
         JMenu colours = new JMenu("Colours");
         JLabel difficultyLabel = new JLabel("Difficulty: " + difficultyPicker(0));
 
+        flags = flagSize[difficulty-1];
+        flagsLabel.setText("Flags left: " + flags);
+
         //JButton tiles[][] = new JButton[gridSize[2*difficulty-2]][gridSize[2*difficulty-1]];
         for (int y = 0; y < gridSize[2*difficulty-2]; y++){
             for (int x = 0; x < gridSize[2*difficulty-1]; x++){
-                //int number = x + 1 + (gridSize[2*difficulty-1] * y);
                 int currentGrid[][] = new int[gridSize[2*difficulty-2]][gridSize[2*difficulty-1]];
                 currentGrid = gridPicker();
                 if(currentGrid[y][x] == 0){
                     tiles[y][x] = new JButton();
-                } else if(currentGrid[y][x] == -3){
-                    tiles[y][x] = new JButton(flagImage);
-                }else{
+                /* } else if(currentGrid[y][x] == -3){
+                    tiles[y][x] = new JButton(flagImage); */
+                } else if(currentGrid[y][x] == 0){
+                    tiles[y][x] = new JButton(uncoveredButton);
+                } else{
                     tiles[y][x] = new JButton(Integer.toString(currentGrid[y][x]));
                 }
                 tiles[y][x].addMouseListener(this);
+                tiles[y][x].setFont(new Font("Serif", Font.PLAIN, 8));
                 if(y%2==0){
                     if(x%2==0){
                         tiles[y][x].setBackground(Color.WHITE);
@@ -131,7 +147,7 @@ class gui implements MouseListener{
         whiteAndGrey.addMouseListener(this);
 
         score.add(reset);
-        score.add(flags);
+        score.add(flagsLabel);
         score.add(time);
         score.add(menu);
         score.add(difficultyLabel);
@@ -140,12 +156,6 @@ class gui implements MouseListener{
         jframe.getContentPane().add(BorderLayout.NORTH, score);
         jframe.getContentPane().add(BorderLayout.CENTER, playArea);
         jframe.setVisible(true);
-        
-        timer = new Timer(1000, new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                updateTimeLabel();
-            }
-        });
     }
 
     private void updateTimeLabel() {
@@ -153,80 +163,139 @@ class gui implements MouseListener{
         time.setText("Time: " + elapsedTime + "s");
     }
 
-    public void plantMines(int xx, int yy){        
+    public void plantMines(int xx, int yy) {
         int z = 0;
-        while (z < flagSize[difficulty-1]+1 && !plantedMines){
-            int randX = (int)Math.floor(Math.random()*gridSize[2*difficulty-1]);
-            int randY = (int)Math.floor(Math.random()*gridSize[2*difficulty-2]);
-            if (difficulty == 1){
-                if (!(easyGrid[randY][randX] == -1) && !(randX == xx && randY == yy)){
-                    easyGrid[randY][randX] = -1;
-                    z++;
-                }
-            } else if (difficulty == 2){
-                if (!(mediumGrid[randY][randX] == -1) && !(randX == xx && randY == yy)){
-                    mediumGrid[randY][randX] = -1;
-                    z++;
-                }
-            } else if (difficulty == 3){
-                if (!(hardGrid[randY][randX] == -1) && !(randX == xx && randY == yy)){
-                    hardGrid[randY][randX] = -1;
-                    z++;
-                }
+        int grid[][] = gridPicker();
+        while (z < flagSize[difficulty - 1] && !plantedMines) {
+            int randX = (int) Math.floor(Math.random() * gridSize[2 * difficulty - 1]);
+            int randY = (int) Math.floor(Math.random() * gridSize[2 * difficulty - 2]);
+            if (!(grid[randY][randX] == -1) && !(randX == xx && randY == yy)) {
+                grid[randY][randX] = -1;
+                z++;
             }
         }
-        if (!plantedMines){
-            gui sm=new gui();
-            sm.guiMethod();
+
+        for (int y = 0; y < gridSize[2 * difficulty - 2]; y++) {
+            for (int x = 0; x < gridSize[2 * difficulty - 1]; x++) {
+                int nearbyMines = 0;
+                if (!(grid[y][x] == -1)) {
+                    if (x > 0) {
+                        if (grid[y][x - 1] == -1) {
+                            nearbyMines++;
+                        }
+                        if (y > 0) {
+                            if (grid[y - 1][x - 1] == -1) {
+                                nearbyMines++;
+                            }
+                        }
+                        if (y + 1 < gridSize[2 * difficulty - 2]) {
+                            if (grid[y + 1][x - 1] == -1) {
+                                nearbyMines++;
+                            }
+                        }
+                    }
+                    if (x + 1 < gridSize[2 * difficulty - 1]) {
+                        if (grid[y][x + 1] == -1) {
+                            nearbyMines++;
+                        }
+                        if (y > 0) {
+                            if (grid[y - 1][x + 1] == -1) {
+                                nearbyMines++;
+                            }
+                        }
+                        if (y + 1 < gridSize[2 * difficulty - 2]) {
+                            if (grid[y + 1][x + 1] == -1) {
+                                nearbyMines++;
+                            }
+                        }
+                    }
+                    if (y > 0) {
+                        if (grid[y - 1][x] == -1) {
+                            nearbyMines++;
+                        }
+                    }
+                    if (y + 1 < gridSize[2 * difficulty - 2]) {
+                        if (grid[y + 1][x] == -1) {
+                            nearbyMines++;
+                        }
+                    }
+                }
+                grid[y][x] = nearbyMines;
+            }
         }
+        switch (difficulty) {
+            case 1:
+                easyGrid = grid;
+                break;
+            case 2:
+                mediumGrid = grid;
+                break;
+            case 3:
+                hardGrid = grid;
+                break;
+            default:
+                break;
+        }
+        guiMethod();
         plantedMines = true;
     }
 
     public void resetGrids(){
         for (int y = 0; y < gridSize[0]; y++){
             for (int x = 0; x < gridSize[1]; x++){
-                easyGrid[y][x] = 0;
+                easyGrid[y][x] = -2;
             }
         }
 
         for (int y = 0; y < gridSize[2]; y++){
             for (int x = 0; x < gridSize[3]; x++){
-                mediumGrid[y][x] = 0;
+                mediumGrid[y][x] = -2;
             }
         }
 
         for (int y = 0; y < gridSize[4]; y++){
             for (int x = 0; x < gridSize[5]; x++){
-                hardGrid[y][x] = 0;
+                hardGrid[y][x] = -2;
             }
         }
+        flags = flagSize[difficulty-1];
+        plantedMines = false;
+        timer.restart();
+        timer.stop();
     }
 
     public void mousePressed(MouseEvent e) {
-        gui sm=new gui();
         int currentGrid[][] = gridPicker();
         for (int y = 0; y < gridSize[2*difficulty-2]; y++) {
             for (int x = 0; x < gridSize[2*difficulty-1]; x++) {
                 if (tiles[y][x] == e.getSource() && e.getButton() == MouseEvent.BUTTON1) {
                     System.out.println("Clicked button at (x, y): (" + (x + 1) + ", " + (y + 1) + ")");
-                    if (!(currentGrid[y][x] == -3)){
+                    if (!(tiles[y][x].getIcon() == flagImage)){
                         tiles[y][x].setText("O");
+                        guiMethod();
+                    } else if (currentGrid[y][x] == 0){
+                        tiles[y][x].setIcon(uncoveredButton);
+                        guiMethod();
                     }
-                    if (plantedMines){
+                    if (!plantedMines){
                         plantMines(x, y);
+                        //System.out.println("past planted mines");
                     }
                     if (!timer.isRunning()) {
                         timer.start();
+                        //System.out.println("past timer start");
                     }
                 } else if (tiles[y][x] == e.getSource() && e.getButton() == MouseEvent.BUTTON3) {
                     System.out.println("Flagged button at (x, y): (" + (x + 1) + ", " + (y + 1) + ")");
-                    if (currentGrid[y][x] == -3){
+                    if (tiles[y][x].getIcon() == flagImage){
                         tiles[y][x].setIcon(null);
-                        currentGrid[y][x] = -2;
+                        flags++;
+                        flagsLabel.setText("Flags left: " + flags);
                     } else{   
-                        currentGrid[y][x] = -3;
                         tiles[y][x].setText("");
                         tiles[y][x].setIcon(flagImage);
+                        flags--;
+                        flagsLabel.setText("Flags left: " + flags);
                     }
                     if (!timer.isRunning()) {
                         timer.start();
@@ -259,18 +328,18 @@ class gui implements MouseListener{
         if (e.getSource() == easy){
             difficulty = 1;
             resetGrids();
-            sm.guiMethod();
+            guiMethod();
         } else if (e.getSource() == medium){
             difficulty = 2;
             resetGrids();
-            sm.guiMethod();
+            guiMethod();
         } else if (e.getSource() == hard){
             difficulty = 3;
             resetGrids();
-            sm.guiMethod();
+            guiMethod();
         } else if (e.getSource() == reset){
             resetGrids();
-            sm.guiMethod();
+            guiMethod();
         }
     }
 
@@ -291,10 +360,8 @@ class gui implements MouseListener{
     }
 
     public static void main(String args[]){
-        gui thing = new gui();
-        thing.resetGrids();
-
         gui sm=new gui();
         sm.guiMethod();
+        sm.resetGrids();
     }
 }
